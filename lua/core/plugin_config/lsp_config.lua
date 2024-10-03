ec6a5854f59a8be712cf2c2161ff2227029f6238
mkamonMdt <mich.kamon91@gmail.com>
2024-10-03 17:33:37 +0200

    telescope vertical setup

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -10,7 +10,6 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
